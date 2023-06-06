1045e38565b49edafb79798930c65a9ce767cf52
JeFeel <jaephil94@gmail.com>
2023-06-07 03:33:19 +0900

    write 경로 수정

--- GS25final.py
+++ GS25final.py
@@ -120,5 +120,5 @@ driver.quit()
 # with open('json_csv/GS25.json', 'w', encoding='utf-8') as f:
 #     json.dump(data, f, ensure_ascii=False, indent='\t')
 
-with open('C:/BackEnd/src/main/resources/static/json_csv/GS25.json', 'w', encoding='utf-8') as f:
+with open('C:/BackEnd-fork/src/main/resources/static/json_csv/GS25.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent='\t')
